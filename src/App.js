8488aaa4e57a19c0e20ba8954ea7cdc8dd9555fb
Peter Hammer Mortensen <67020755+remmahdk@users.noreply.github.com>
2021-01-20 14:31:12 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>easy react starter plate</h1>
+        </header>
+        <div className="loader">
+            <span className="loaderLeft"></span>React<span className="loaderRight"></span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
