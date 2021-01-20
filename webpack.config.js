bb7efa7b746612fa0de03fefc144ba2f486b596d
Peter Hammer Mortensen <67020755+remmahdk@users.noreply.github.com>
2021-01-20 14:37:05 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+    "mode": "development",
+    "entry": "./src/index.js",
+    "output": {
+        "path": __dirname + '/dir',
+        "filename": "bundle.js"
+    },
+    devServer: {
+        inline:true,
+        host: '0.0.0.0',
+        port: 3002
+    },
+    "devtool": "source-map",
+    "module": {
+        "rules": [
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader"
+                }
+            },
+            {
+                "test": /\.css$/,
+                "use": [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+	        { 
+                test: /\.s[ac]ss$/i,
+                    use: [
+                    // Creates 'style' nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                "test": /\.html$/,
+                "use": [
+                    {
+                        "loader": "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    "plugins": [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
