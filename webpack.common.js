b8d5a95cf7c549c563cfc40a6d7df23814496964
Peter Hammer Mortensen <67020755+remmahdk@users.noreply.github.com>
2021-01-21 01:26:32 +0100

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -14,6 +14,43 @@
        title: 'Production'
      })
    ],
+   "module": {
+        "rules": [
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader"
+                }
+            },
+            {
+                "test": /\.css$/,
+                "use": [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+	    { 
+                "test": /\.s[ac]ss$/i,
+                "use": [
+                    // Creates 'style' nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                "test": /\.html$/,
+                "use": [
+                    {
+                        "loader": "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
    output: {
      filename: '[name].bundle.js',
      path: path.resolve(__dirname, 'dist')
