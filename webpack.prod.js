5d1cb7f91f5219376dd0e087c317aa6173b114ac
Peter Hammer Mortensen <67020755+remmahdk@users.noreply.github.com>
2021-01-21 01:20:23 +0100

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,7 @@
- module.exports = require('webpack-merge')(require('./webpack.common.js'), {
+ const { merge } = require('webpack-merge');
+ const common = require('./webpack.common.js');
+
+ module.exports = merge(common, {
    mode: 'production',
    devtool: 'source-map'
  });
